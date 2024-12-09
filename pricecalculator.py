42ff570624630eca20463adbceaa5ce73e93d696
Goderdzi Gadelia <130245892+GadeliaGoderdzi@users.noreply.github.com>
2024-12-09 02:21:40 -0500

    Update pricecalculator.py

--- pricecalculator.py
+++ pricecalculator.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("My First Streamlit App")
+st.title("APMT Poti Price Calculator")
 st.header("Welcome!")
 st.write("Hello, World! This app was created using GitHub and Streamlit.")
